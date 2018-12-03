8c9a2cd02e261579fa106fbb6cb0359363ed8d46
panshangqi <1187816874@qq.com>
2018-12-04 01:45:02 +0800

    console

--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,14 @@ const path = require('path')
 const fs = require('fs')
 const yq_addon = require('window_file_class')
 const { logger } = yq_addon
-
+chrome.developerPrivate.openDevTools({
+    renderViewId: -1,
+    renderProcessId: -1,
+    extensionId: chrome.runtime.id
+});
+process.on('uncaughtException', (err) => {
+    console.error(err)
+});
 function main(win, gui) {
     var appdata = gui.App.dataPath
     logger.info(appdata)
@@ -12,10 +19,24 @@ function main(win, gui) {
     var cwdPath = process.cwd()
     logger.info(cwdPath)
 
+    console.log('xxxxxxxxxxxx')
 
     logger.info('******************************')
 }
+var gui = window.require('nw.gui');  // Load native UI library.
+//var url = 'http://127.0.0.1:10032/templates/index.html'
+var url = 'http://www.baidu.com'
 
+var win = nw.Window.open(url, {
+    width: 1300,
+    height: 720
+}, function(win) {
+    win.showDevTools()
+
+    main(win, gui)
+    sys.tray.create(win, gui)
+});
+/*
 exports.init = function() {
     var gui = window.require('nw.gui');  // Load native UI library.
     //var url = 'http://127.0.0.1:10032/templates/index.html'
@@ -32,4 +53,4 @@ exports.init = function() {
     });
 
 };
-
+*/
